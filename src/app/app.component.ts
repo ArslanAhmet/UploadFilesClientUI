911fce0f764064ec8b26c998aa1bfa8aa69dbe13
Ahmet Arslan <ahmet.arslan@inspiratintech.co.uk>
2023-03-08 12:49:09 +0300

    some unnecessary files removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { UserToCreate } from './_interfaces/userToCreate.model';
-import { User } from './_interfaces/user.model';
+
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
@@ -12,40 +11,18 @@ export class AppComponent implements OnInit {
   isCreate: boolean;
   name: string;
   address: string;
-  user: UserToCreate;
-  users: User[] = [];
-  response: {dbPath: ''};
+  response: { dbPath: '' };
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.isCreate = true;
   }
 
   onCreate = () => {
-    this.user = {
-      name: this.name,
-      address: this.address,
-      imgPath: this.response.dbPath
-    }
 
-    this.http.post('https://localhost:5001/api/users', this.user)
-    .subscribe({
-      next: _ => {
-        this.getUsers();
-        this.isCreate = false;
-      },
-      error: (err: HttpErrorResponse) => console.log(err)
-    });
   }
 
-  private getUsers = () => {
-    this.http.get('https://localhost:5001/api/users')
-    .subscribe({
-      next: (res) => this.users = res as User[],
-      error: (err: HttpErrorResponse) => console.log(err)
-    });
-  }
 
   returnToCreate = () => {
     this.isCreate = true;
@@ -53,11 +30,11 @@ export class AppComponent implements OnInit {
     this.address = '';
   }
 
-  uploadFinished = (event) => { 
-    this.response = event; 
+  uploadFinished = (event) => {
+    this.response = event;
   }
 
-  public createImgPath = (serverPath: string) => { 
-    return `https://localhost:5001/${serverPath}`; 
+  public createImgPath = (serverPath: string) => {
+    return `https://localhost:5001/${serverPath}`;
   }
 }
--- src/app/model/person-item.ts
+++
@@ -1,8 +0,0 @@
-export class PersonItem {
-    public id!: number ;
-    public Name!: string;
-    public Language!: string;
-    public Type!: number ;
-    public Email!: string;
-  }
-  
